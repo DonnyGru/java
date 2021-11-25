b43d33eadc5422432a7e822b5aa4c436be814835
DonnyGru <91632259+DonnyGru@users.noreply.github.com>
2021-11-25 16:40:58 +0100

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,6 @@
+console.log("Hello Winc Academy students")
+/* var x=1230941*1823794;
+// This is a large sum: //
+console.log("Multiplication result:",x);
+var y=637263/54;
+console.log("Division result:",y); */
